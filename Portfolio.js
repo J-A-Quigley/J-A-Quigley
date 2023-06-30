6f713fab28028224c68ec8ad2ac20f9745c88926
J-A-Quigley <86603148+J-A-Quigley@users.noreply.github.com>
2023-06-30 09:15:48 +0100

    Mobile Version

--- Portfolio.js
+++ Portfolio.js
@@ -1,3 +1,9 @@
-function Abo() {
-	document.getElementById("About").innerHTML = "Here's a bunch of stuff about me";
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("Links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
