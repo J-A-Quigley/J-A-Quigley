dabd192c27321fdb34e2a80e4b49eb4bda3be10b
J-A-Quigley <86603148+J-A-Quigley@users.noreply.github.com>
2023-07-09 18:59:32 +0100

    Updated Portfolio

--- Portfolio.js
+++ Portfolio.js
@@ -1,3 +1,4 @@
+/*Made By Jack Alexander Quigley*/
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("Links");
